8b2b34e6f0870e225e294e5a229e28da67cab280
Wasswa Richard <wasswa@awamo.com>
2021-07-03 09:41:37 +0300

    project set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './styles/styles.sass'
-
-// import IMAGE from './download.png';
+import Download from './download.png'
 export const App = () => {
   return (
     <>
       <h1> React TypeScript React DataTables</h1>
-      {/*<img src={IMAGE} alt="image" width="300" height="200"/>*/}
+      <img src={Download} alt="download" width="300" height="200" />
     </>
   )
 }
