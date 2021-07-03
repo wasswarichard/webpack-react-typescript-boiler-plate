3e727da67dc6df6757404bbe7b24a0ea54cea83f
Wasswa Richard <wasswa@awamo.com>
2021-07-03 09:53:04 +0300

    project set up

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,12 +1,10 @@
 const webpack = require('webpack')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = {
     mode: 'production',
     devtool: 'source-map',
     plugins: [
         new webpack.DefinePlugin({
             'process.env.name': JSON.stringify('datatable'),
-        }),
-        new BundleAnalyzerPlugin(),
+        })
     ],
 }
